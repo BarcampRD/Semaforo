318389cbec43c6fe9ef6d47157ff38045d2973df
vacax <ccamachog@avathartech.com>
2018-11-16 14:11:07 -0400

    Agregando parametro para cambio del puerto.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -9,12 +9,18 @@ import static spark.Spark.*;
 
 public class Main {
 
+    static int puerto = 4569;
+
     public static void main(String[] args) {
         staticFileLocation("/");
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_23);
         configuration.setClassForTemplateLoading(Main.class, "/templates");
         FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine(configuration);
-        port(4569);
+        if(args.length > 0){
+           puerto =  Integer.parseInt(args[0]);
+        }
+        System.out.println("Puerto por indicado: "+puerto);
+        port(puerto);
 
         get("/", (request, response) -> new ModelAndView(null, "index.html"), freeMarkerEngine);
     }
